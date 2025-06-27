a3ae8b075ba5a1ec1e62d63aa35cd323186ffce5
jimjrxieb <jimmie012506@gmail.com>
2025-06-26 22:03:43 -0400

    still trying

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = var.dns_prefix
+  kubernetes_version  = "1.27.9"
 
   default_node_pool {
     name       = "default"
